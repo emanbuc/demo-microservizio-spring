49b6825c1d50742d691a8eb47f575c7d9c77dec4
emanuele.buchicchio <emanuele.buchicchio@smartpeg.eu>
2023-06-05 20:54:40 +0200

    Initial commit

---
+++ src/main/java/com/example/demomicroserviziospring/DemoMicroservizioSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.demomicroserviziospring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoMicroservizioSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoMicroservizioSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demomicroserviziospring/DemoMicroservizioSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demomicroserviziospring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoMicroservizioSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
